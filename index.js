715fe3491e7d0da258193718b33221a8935d2738
br3ntor <br3ntor@gmail.com>
2022-04-29 19:26:42 -0700

    Updated my queries.

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  db.all("SELECT * FROM players", (err, rows) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    if (rows) {
-      res.send(rows);
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 // Selecting all player files (more than one are possible)
 // It seems like the last in list is most recent file
 const file = fs
@@ -33,6 +16,12 @@ const file = fs
   .slice(-1);
 
 const filePath = "/home/pzserver/Zomboid/Logs/" + file;
+
+db.serialize(() => {
+  db.run("DELETE FROM players;", () => {
+    console.log("players cleared");
+  });
+});
 
 // Check if the file exists in the current directory.
 fs.access(filePath, fs.constants.F_OK, (err) => {
@@ -60,47 +49,65 @@ fs.access(filePath, fs.constants.F_OK, (err) => {
           return;
         }
 
-        // Parse data from log into desired format.
-        const curlyb_match = results.match(/\{([^}]+)\}/g);
-        const squareb_match = results.match(/\[([^\]]+)\]/g);
+        // If our line is a tick event then we enter into database
+        if (results.split(" ")[5] === "tick") {
+          // Parse data from log into desired format.
+          const curlyb_match = results.match(/\{([^}]+)\}/g);
+          const squareb_match = results.match(/\[([^\]]+)\]/g);
 
-        const player = {
-          $name: results.split(" ")[4].replace(/['"]+/g, ""),
-          $perks: curlyb_match[0],
-          $traits: squareb_match[1],
-          $stats: curlyb_match[1],
-          $health: curlyb_match[2],
-        };
+          const player = {
+            $name: results.split(" ")[4].replace(/['"]+/g, ""),
+            $perks: curlyb_match[0],
+            $traits: squareb_match[1],
+            $stats: curlyb_match[1],
+            $health: curlyb_match[2],
+          };
 
-        // Insert player data if it doesnt exist, otherwise update the data.
-        db.get(
-          "SELECT * FROM players WHERE name = ?",
-          player["$name"],
-          (err, rows) => {
-            if (err) {
-              console.log(err);
-              return;
+          // Insert player data if it doesnt exist, otherwise update the data.
+          db.get(
+            "SELECT * FROM players WHERE name = ?",
+            player["$name"],
+            (err, rows) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+
+              if (rows) {
+                db.run(
+                  "UPDATE players SET perks = $perks, traits = $traits, stats = $stats, health = $health WHERE name = $name",
+                  player
+                );
+              } else {
+                db.run(
+                  "INSERT INTO players (name, perks, traits, stats, health) VALUES ($name, $perks, $traits, $stats, $health) ON CONFLICT(name) DO UPDATE SET perks = $perks, traits = $traits, stats = $stats, health = $health",
+                  player
+                );
+              }
             }
-
-            if (rows) {
-              db.run(
-                "UPDATE players SET perks = $perks, traits = $traits, stats = $stats, health = $health WHERE name = $name",
-                player
-              );
-            } else {
-              db.run(
-                "INSERT INTO players (name, perks, traits, stats, health) VALUES ($name, $perks, $traits, $stats, $health)",
-                player
-              );
-            }
-          }
-        );
-
-        // console.log(player);
+          );
+        }
         console.log("---end-tick---");
       });
     } else {
       console.log("filename not provided");
     }
   });
+});
+
+app.get("/", (req, res) => {
+  db.all("SELECT * FROM players", (err, rows) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    if (rows) {
+      res.send(rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
