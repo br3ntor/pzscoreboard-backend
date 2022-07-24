694ff792fcc24fcaa7957a9779f61241e8c5658a
brent <br3ntor@gmail.com>
2022-07-24 00:56:48 -0700

    Update two db for two servers.

---
+++ helpers.js
@@ -0,0 +1,75 @@
+const sqlite3 = require("sqlite3").verbose();
+const dbLight = new sqlite3.Database("./database/lightplayers.db");
+const dbHeavy = new sqlite3.Database("./database/heavyplayers.db");
+
+// Insert player data if it doesnt exist, otherwise update the data.
+function insertOrUpdate(player, db) {
+  db.get(
+    "SELECT * FROM players WHERE name = ?",
+    player["$name"],
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (rows) {
+        db.run(
+          "UPDATE players SET perks = $perks, traits = $traits, stats = $stats, health = $health WHERE name = $name",
+          player
+        );
+      } else {
+        db.run(
+          "INSERT INTO players (name, perks, traits, stats, health) VALUES ($name, $perks, $traits, $stats, $health) ON CONFLICT(name) DO UPDATE SET perks = $perks, traits = $traits, stats = $stats, health = $health",
+          player
+        );
+      }
+    }
+  );
+}
+
+/**
+ * Parse player tick string into object for db entry.
+ */
+function createPlayerObj(playerTick) {
+  const curlyb_match = playerTick.match(/\{([^}]+)\}/g);
+  const squareb_match = playerTick.match(/\[([^\]]+)\]/g);
+
+  return {
+    $name: playerTick.split(/"(?!\d)(.*?)"/s)[1],
+    $perks: curlyb_match[0],
+    $traits: squareb_match[1],
+    $stats: curlyb_match[1],
+    $health: curlyb_match[2],
+  };
+}
+
+// Callback for exec function
+function updateOnTick(error, results) {
+  if (error) {
+    throw error;
+  }
+
+  if (!results) {
+    console.log(results);
+    console.log("no results?");
+    return;
+  }
+
+  // If our line is a tick event then we enter into database
+  const lineIsTick = results.split('"')[2].trim().split(" ")[0] === "tick";
+
+  if (lineIsTick) {
+    const player = createPlayerObj(results);
+    const dbToUpdate =
+      results.split("/")[2] === "pzserverlight" ? dbLight : dbHeavy;
+
+    insertOrUpdate(player, dbToUpdate);
+  }
+
+  console.log(results);
+}
+
+module.exports = {
+  updateOnTick,
+};
--- index.js
+++ index.js
@@ -1,108 +1,79 @@
 const fs = require("fs");
 const { exec } = require("child_process");
 
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./database/db.sqlite");
-
-const https = require("https");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const cors = require("cors");
 
+const sqlite3 = require("sqlite3").verbose();
+const dbLight = new sqlite3.Database("./database/lightplayers.db");
+const dbHeavy = new sqlite3.Database("./database/heavyplayers.db");
+
+const helpers = require("./helpers");
+
 // Selecting all player files (more than one are possible)
 // It seems like the last in list is most recent file
-const file = fs
-  .readdirSync("/home/pzserver/Zomboid/Logs")
+const heavyFileName = fs
+  .readdirSync("/home/pzserverheavy/Zomboid/Logs")
   .filter((fileName) => fileName.endsWith("player.txt"))
   .slice(-1);
+const heavyFilePath = "/home/pzserverheavy/Zomboid/Logs/" + heavyFileName;
 
-const filePath = "/home/pzserver/Zomboid/Logs/" + file;
+const lightFileName = fs
+  .readdirSync("/home/pzserverlight/Zomboid/Logs")
+  .filter((fileName) => fileName.endsWith("player.txt"))
+  .slice(-1);
+const lightFilePath = "/home/pzserverlight/Zomboid/Logs/" + lightFileName;
 
-// db.serialize(() => {
-//   db.run("DELETE FROM players;", () => {
-//     console.log("players cleared");
-//   });
-// });
+// Starts watching a player log file from light or heavy server
+// Updates db when newline is written to log and that line is a tick
+function logTickDBUpdate(filePath, updateFunc) {
+  // Check if the file exists in the current directory.
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw err;
+    }
 
-// Check if the file exists in the current directory.
-fs.access(filePath, fs.constants.F_OK, (err) => {
-  if (err) {
-    throw err;
-  }
+    console.log("File exists, starting watch...");
 
-  console.log("File exists, starting watch...");
+    fs.watch(filePath, (eventType, filename) => {
+      console.log("---start-tick---");
+      console.log(`event type is: ${eventType}`);
 
-  fs.watch(filePath, (eventType, filename) => {
-    console.log("---start-tick---");
-    console.log(`event type is: ${eventType}`);
+      if (filename) {
+        console.log(`filename provided: ${filename}`);
+        exec(`wc -l ${filePath};tail -1 ${filePath}`, updateFunc);
+      } else {
+        console.log("filename not provided");
+      }
+    });
+  });
+}
 
-    if (filename) {
-      console.log(`filename provided: ${filename}`);
-      exec(`wc -l ${filePath};tail -1 ${filePath}`, (error, results) => {
-        if (error) {
-          throw error;
-        }
+// Start the watcher updaters.
+logTickDBUpdate(heavyFilePath, helpers.updateOnTick);
+logTickDBUpdate(lightFilePath, helpers.updateOnTick);
 
-        if (!results) {
-          console.log(results);
-          console.log("no results?");
-          return;
-        }
+app.use(cors());
 
-        // If our line is a tick event then we enter into database
-        const lineIsTick =
-          results.split('"')[2].trim().split(" ")[0] === "tick";
-        if (lineIsTick) {
-          // Parse data from log into desired format.
-          const curlyb_match = results.match(/\{([^}]+)\}/g);
-          const squareb_match = results.match(/\[([^\]]+)\]/g);
+// These two callbacks can be abstracted
+app.get("/light", (req, res) => {
+  dbLight.all("SELECT * FROM players", (err, rows) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-          const player = {
-            $name: results.split(/"(?!\d)(.*?)"/s)[1],
-            $perks: curlyb_match[0],
-            $traits: squareb_match[1],
-            $stats: curlyb_match[1],
-            $health: curlyb_match[2],
-          };
-
-          // Insert player data if it doesnt exist, otherwise update the data.
-          db.get(
-            "SELECT * FROM players WHERE name = ?",
-            player["$name"],
-            (err, rows) => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-
-              if (rows) {
-                db.run(
-                  "UPDATE players SET perks = $perks, traits = $traits, stats = $stats, health = $health WHERE name = $name",
-                  player
-                );
-              } else {
-                db.run(
-                  "INSERT INTO players (name, perks, traits, stats, health) VALUES ($name, $perks, $traits, $stats, $health) ON CONFLICT(name) DO UPDATE SET perks = $perks, traits = $traits, stats = $stats, health = $health",
-                  player
-                );
-              }
-            }
-          );
-        }
-        console.log("---end-tick---");
-      });
-    } else {
-      console.log("filename not provided");
+    if (rows) {
+      res.send(rows);
     }
   });
 });
 
-app.use(cors());
-
-app.get("/", (req, res) => {
-  db.all("SELECT * FROM players", (err, rows) => {
+app.get("/heavy", (req, res) => {
+  dbHeavy.all("SELECT * FROM players", (err, rows) => {
     if (err) {
       console.log(err);
       return;
